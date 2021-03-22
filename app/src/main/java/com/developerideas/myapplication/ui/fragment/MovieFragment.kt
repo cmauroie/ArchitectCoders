f5932d703baf21e42f34914411d957e57248ba56
Francisco Puma <fppuma@gmail.com>
2021-03-22 15:01:26 -0500

    14 movie fragment, adapter and item

--- app/src/main/java/com/developerideas/myapplication/ui/fragment/MovieFragment.kt
+++ app/src/main/java/com/developerideas/myapplication/ui/fragment/MovieFragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.developerideas.myapplication.R
+import com.developerideas.myapplication.databinding.FragmentMovieBinding
+import com.developerideas.myapplication.model.Movie
+import com.developerideas.myapplication.view.adapter.MovieAdapter
+import com.developerideas.myapplication.viewmodel.MovieViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +27,54 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MovieFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var movieAadapter : MovieAdapter
+    private lateinit var viewModel: MovieViewModel
+
+    private lateinit var fragmentMovieBinding: FragmentMovieBinding;
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fragmentMovieBinding = FragmentMovieBinding.inflate(inflater,container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        //return inflater.inflate(R.layout.fragment_movie, container, false)
+        return fragmentMovieBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MovieFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MovieFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
+        viewModel.refresh()
+
+        movieAadapter = MovieAdapter()
+
+        fragmentMovieBinding.rvMovieFragment.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            adapter = movieAadapter
+        }
+        observeViewModel()
+
+
     }
+
+    private fun observeViewModel() {
+        viewModel.movieList.observe(this, Observer<List<Movie>> { schedule ->
+            movieAadapter.updateData(schedule)
+        })
+
+
+    }
+
+
 }
