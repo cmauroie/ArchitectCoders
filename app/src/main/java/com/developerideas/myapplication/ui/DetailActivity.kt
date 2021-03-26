cd287c5ad1e4f819635e4884d707654b90d47327
Juan Carlos Ortiz H <juanortizmg@gmail.com>
2021-03-25 21:00:50 -0500

    Add logic in MainActivity.kt and create depending files

--- app/src/main/java/com/developerideas/myapplication/ui/DetailActivity.kt
+++ app/src/main/java/com/developerideas/myapplication/ui/DetailActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import com.developerideas.myapplication.R
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val MOVIE = "DetailActivity:movie"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
--- app/src/main/java/com/developerideas/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/developerideas/myapplication/ui/MainActivity.kt
@@ -2,11 +2,30 @@ package com.developerideas.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.developerideas.myapplication.R
+import com.developerideas.myapplication.databinding.ActivityMainBinding
+import com.developerideas.myapplication.model.MoviesRepository
+import com.developerideas.myapplication.ui.common.CoroutineScopeActivity
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CoroutineScopeActivity() {
+
+    private val moviesRepository: MoviesRepository by lazy { MoviesRepository(this) }
+
+    private val adapter = MoviesAdapter {
+        startActivity<DetailActivity> {
+            putExtra(DetailActivity.MOVIE, it)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recycler.adapter = adapter
+
+        launch {
+            adapter.movies = moviesRepository.findPopularMovies().results
+        }
     }
 }
