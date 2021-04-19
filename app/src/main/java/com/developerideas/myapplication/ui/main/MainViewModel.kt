55c3a76f242a793d6e56f2ca40d6bca7aab2d9c8
Juan Carlos Ortiz H <juanortizmg@gmail.com>
2021-04-18 22:47:04 -0500

    Convert MainPresenter.kt to MainViewModel.kt and add PermissionRequester.kt

---
+++ app/src/main/java/com/developerideas/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,61 @@
+package com.developerideas.myapplication.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.developerideas.myapplication.model.Movie
+import com.developerideas.myapplication.model.MoviesRepository
+import com.developerideas.myapplication.ui.common.Scope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val moviesRepository: MoviesRepository) : ViewModel(), Scope by Scope.Impl() {
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+        get() {
+            if (_model.value == null) refresh()
+            return _model
+        }
+
+    sealed class UiModel {
+        object Loading : UiModel()
+        class Content(val movies: List<Movie>) : UiModel()
+        class Navigation(val movie: Movie) : UiModel()
+        object RequestLocationPermission : UiModel()
+    }
+
+    init {
+        initScope()
+    }
+
+    private fun refresh() {
+        launch {
+            _model.value = UiModel.Loading
+            _model.value = UiModel.Content(moviesRepository.findPopularMovies().results)
+        }
+    }
+
+    fun onCoarsePermissionRequester() {
+        launch {
+            _model.value = UiModel.Loading
+            _model.value = UiModel.Content(moviesRepository.findPopularMovies().results)
+        }
+    }
+
+    fun onMovieClicked(movie: Movie) {
+        _model.value = UiModel.Navigation(movie)
+    }
+
+    override fun onCleared() {
+        destroyScope()
+        super.onCleared()
+    }
+
+}
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private val moviesRepository: MoviesRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        MainViewModel(moviesRepository) as T
+}
