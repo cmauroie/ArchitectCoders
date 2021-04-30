bfb545a30941568f26d53b50a537c9b1f9ebef70
JaimeBallesta <jaime.ballesta@globant.com>
2021-04-30 16:55:14 -0500

    add inline function for optimize the factories

--- app/src/main/java/com/developerideas/myapplication/ui/common/Extensions.kt
+++ app/src/main/java/com/developerideas/myapplication/ui/common/Extensions.kt
@@ -1,4 +1,4 @@
-package com.developerideas.myapplication.ui
+package com.developerideas.myapplication.ui.common
 
 import android.app.Activity
 import android.content.Context
@@ -9,6 +9,10 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -19,14 +23,14 @@ fun Context.toast(message: String) {
 }
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
-    LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+        LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
 
 fun ImageView.loadUrl(url: String) {
     Glide.with(context).load(url).into(this)
 }
 
 inline fun <reified T : Activity> Context.intentFor(body: Intent.() -> Unit): Intent =
-    Intent(this, T::class.java).apply(body)
+        Intent(this, T::class.java).apply(body)
 
 inline fun <reified T : Activity> Context.startActivity(body: Intent.() -> Unit) {
     startActivity(intentFor<T>(body))
@@ -50,3 +54,12 @@ inline fun <VH : RecyclerView.ViewHolder, T> RecyclerView.Adapter<VH>.basicDiffU
                 override fun getNewListSize(): Int = new.size
             }).dispatchUpdatesTo(this@basicDiffUtil)
         }
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(crossinline factory: () -> T): T {
+    val vmFactory = object : ViewModelProvider.Factory {
+        override fun <U : ViewModel?> create(modelClass: Class<U>): U = factory() as U
+    }
+
+    return ViewModelProvider(this, vmFactory).get()
+}
